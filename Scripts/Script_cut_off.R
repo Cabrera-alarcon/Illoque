a3970a3927faa274ce739f0c62ab8f6c0fb0ae0a
Cabrera-alarcon <joseluiscabreraalarcon@gmail.com>
2018-12-28 17:17:48 +0100

    Add files via upload

--- Scripts/Script_cut_off.R
+++ Scripts/Script_cut_off.R
@@ -6,47 +6,47 @@
 
 ### You will have to run VSRFM.R and VSRFMs.R before
 
-### Plot VSRFM scores in neutral and pathologic training not splice variants
+### Plot VSRFM scores in neutral and pathologic Clinvar not splice variants
 
 par(mfrow=c(1,2))
 
 ## Separate benign and deleterious variants in twoo different data sets
 
-table.model.NS.neutra=table.model.NS[which(table.model.NS$True.Label==0),]
-table.model.NS.deleterious=table.model.NS[which(table.model.NS$True.Label==1),]
+clinvar_NS.neutra=clinvar_NS[which(clinvar_NS$True.Label==0),]
+clinvar_NS.deleterious=clinvar_NS[which(clinvar_NS$True.Label==1),]
 
 ## Density plot for VSRFM scores in neutral and pathological 
 
-plot(density(predict(model.rf,table.model.NS.neutra)), col='blue', ylim=c(0,30),
+plot(density(predict(model.rf,clinvar_NS.neutra)), col='blue', ylim=c(0,30),
      xlim=c(0,1),main='score VSRFM cutoff',lty=3, xlab='')
 
 par(new=T)
 
-plot(density(predict(model.rf,table.model.NS.deleterious)), col='red', ylim=c(0,30),
+plot(density(predict(model.rf,clinvar_NS.deleterious)), col='red', ylim=c(0,30),
      xlim=c(0,1),main='score VSRFM cutoff', lty=2, xlab='')
 
-segments(x0 = 0.4599167, y0 = 0, x1 = 0.4599167, y1 = 8)
-text(x = 0.4599167, y = 9, labels = 'cutoff = 0.459')
+segments(x0 = 0.4433667, y0 = 0, x1 = 0.4433667, y1 = 8)
+text(x = 0.4433667, y = 9, labels = 'cutoff = 0.443')
 text(x = (-0.3), y = 40, labels = "A", xpd = NA, cex = 1.5, font = 2)
 
 ### Plot VSRFM scores in neutral and pathologic training splice variants
 
 ## Separate benign and deleterious variants in twoo different data sets
 
-table.model.splicing.neutra=table.model.splicing[which(table.model.splicing$True.Label==0),]
-table.model.splicing.deleterious=table.model.splicing[which(table.model.splicing$True.Label==1),]
+clinvar_splice.neutra=clinvar_splice[which(clinvar_splice$True.Label==0),]
+clinvar_splice.deleterious=clinvar_splice[which(clinvar_splice$True.Label==1),]
 
 ## Density plot for VSRFMs scores in neutral and pathological training splice variants
 
-plot(density(predict(model.rf.splicing,table.model.splicing.neutra)), col='blue', ylim=c(0,30),
+plot(density(predict(model.rf.splicing,clinvar_splice.neutra)), col='blue', ylim=c(0,30),
      xlim=c(0,1),main='score VSRFM-s cutoff',lty=3, xlab='')
 
 par(new=T)
 
-plot(density(predict(model.rf.splicing,table.model.splicing.deleterious)), col='red', ylim=c(0,30),
+plot(density(predict(model.rf.splicing,clinvar_splice.deleterious)), col='red', ylim=c(0,30),
      xlim=c(0,1),main='score VSRFM-s cutoff', lty=2, xlab='')
 
-segments(x0 = 0.5079833, y0 = 0, x1 = 0.5079833, y1 = 8)
-text(x = 0.5079833, y = 9, labels = 'cutoff = 0.508')
+segments(x0 = 0.35543333, y0 = 0, x1 = 0.35543333, y1 = 8)
+text(x = 0.35543333, y = 9, labels = 'cutoff = 0.355')
 text(x = (-0.3), y = 40, labels = "B", xpd = NA, cex = 1.5, font = 2)
 
